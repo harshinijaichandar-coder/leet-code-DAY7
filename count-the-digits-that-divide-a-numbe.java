250939c94fd51b6188f4cb427d208bc6d34dbb33
harshinijaichandar-coder <harshinijaichandar@gmail.com>
2026-07-01 23:57:07 +0530

    Create count-the-digits-that-divide-a-numbe.java

---
+++ count-the-digits-that-divide-a-numbe.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countDigits(int num) {
+        int count=0,temp=num;
+        while(num!=0){
+            int digit=num%10;
+            if(temp%digit==0){
+                count++;
+            }
+            num=num/10;
+        }
+        return count;
+        
+    }
+}
